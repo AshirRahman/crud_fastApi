8eec8ab6fdfb2d4b64090f50c5e5d7ad444680eb
AshirRahman <123103184+AshirRahman@users.noreply.github.com>
2025-10-07 16:33:05 +0600

    get & post method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
@@ -6,6 +7,26 @@ app = FastAPI()
 def greet():
     return "Welcome to flutter development"
 
+products = [
+    Product(id=1, name="Laptop", description="A high-performance laptop", price=999.99, quantity=10),
+    Product(id=2, name="Smartphone", description="A latest model smartphone", price=699.99, quantity=25),
+    Product(id=3, name="Headphones", description="Noise-cancelling headphones", price=199.99, quantity=15),
+    Product(id=6, name="Headphones", description="Noise-cancelling headphones", price=199.99, quantity=15),
+]
+
 @app.get("/products")
 def get_all_products():
-    return "all products"
+    return products
+
+@app.get("/product/{id}")
+def get_product_by_id(id: int):
+    for product in products:
+        if product.id == id:
+            return product
+        
+    return "product not found"
+
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return products
